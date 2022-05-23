18bd62688c8887f09b53fa35e173d3f3a9840474
Lin Onetwo <linonetwo012@gmail.com>
2022-05-23 23:52:45 +0800

    feat: allow useFilter passin widget

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState, useMemo } from 'react';
+import { Widget } from 'tiddlywiki';
 
 /** Filter result is basically a list of title. You can get tiddler detail using tw API like `$tw.wiki.getTiddler` */
-export function useFilter(twFilter: string, dependencies: any[] = []) {
+export function useFilter(twFilter: string, widget: Widget = $tw.rootWidget, dependencies: unknown[] = []): string[] {
   const [filterResult, setFilterResult] = useState<string[]>([]);
   const compiledFilter = useMemo(() => $tw.wiki.compileFilter(twFilter), [twFilter]);
   useEffect(() => {
-    setFilterResult(compiledFilter());
-  }, [compiledFilter, ...dependencies]);
+    setFilterResult(compiledFilter(undefined, widget));
+  }, [compiledFilter, widget, ...dependencies]);
   return filterResult;
 }
