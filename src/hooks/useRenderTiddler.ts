f6551aadb3b7417cbbc900fa5839d961aeca9bfa
Lin Onetwo <linonetwo012@gmail.com>
2022-11-30 01:18:10 +0800

    Update useRenderTiddler.ts

--- src/hooks/useRenderTiddler.ts
+++ src/hooks/useRenderTiddler.ts
@@ -1,22 +1,44 @@
 import { RefObject, useContext, useEffect } from 'react';
 import { ParentWidgetContext } from './context';
 
-export function useRenderTiddler(tiddlerTitle: string, containerRef: RefObject<HTMLDivElement>) {
+interface IOptions {
+  /** skip rendering for this time useEffect executes */
+  skip?: boolean;
+}
+export function useRenderTiddler(tiddlerTitle: string, containerReference: RefObject<HTMLDivElement>, options?: IOptions) {
   const parentWidget = useContext(ParentWidgetContext);
   useEffect(() => {
-    if (containerRef.current === null || parentWidget === undefined) {
+    const domNode = containerReference.current;
+    if (domNode === null) {
       return;
     }
+    if (parentWidget === undefined) {
+      throw new Error(
+        'Your plugin have a bug: `parentWidget` is undefined, you should use `<ParentWidgetContext.Provider value={props.parentWidget}>`, see tw-react for document.',
+      );
+    }
+    if (options?.skip === true) {
+      return;
+    }
+    const id = String(Math.random());
     const transcludeWidgetNode = $tw.wiki.makeTranscludeWidget(tiddlerTitle, {
       document,
       parentWidget,
       recursionMarker: 'yes',
       mode: 'block',
       importPageMacros: true,
+      variables: { 'use-widget-id': id },
     });
     const tiddlerContainer = document.createElement('div');
-    containerRef.current.append(tiddlerContainer);
+    domNode.append(tiddlerContainer);
     transcludeWidgetNode.render(tiddlerContainer, null);
     parentWidget.children.push(transcludeWidgetNode);
-  }, [tiddlerTitle, containerRef.current]);
+    return () => {
+      parentWidget.children = parentWidget.children.filter((child) => child.getVariable('use-widget-id') !== id);
+      if (domNode === null) {
+        return;
+      }
+      domNode.textContent = '';
+    };
+  }, [tiddlerTitle, containerReference]);
 }
