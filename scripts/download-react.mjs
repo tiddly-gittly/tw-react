e5d47b102247ab1c5c5d99622d65f0fcc44ce139
lin onetwo <linonetwo012@gmail.com>
2024-12-19 02:59:58 +0800

    lint

--- scripts/download-react.mjs
+++ scripts/download-react.mjs
@@ -15,31 +15,42 @@ const reactPath = `node_modules/react/cjs`;
 const reactDomPath = `node_modules/react-dom/cjs`;
 const schedulerPath = `node_modules/.pnpm/scheduler@0.25.0/node_modules/scheduler/cjs`;
 
-const [reactDev, react, reactDomDev, reactDom, reactDomClientDev, reactClientDom, reactJsxRuntimeDev, reactJsxRuntime, schedulerDev, scheduler] = await Promise.all([
-  fs.readFile(path.join(reactPath, `react.development.js`), 'utf-8').then(fixProcessUndefined),
-  fs.readFile(path.join(reactPath, `react.production.js`), 'utf-8'),
-  fs.readFile(path.join(reactDomPath, `react-dom.development.js`), 'utf-8').then(fixProcessUndefined),
-  fs.readFile(path.join(reactDomPath, `react-dom.production.js`), 'utf-8'),
-  fs.readFile(path.join(reactDomPath, `react-dom-client.development.js`), 'utf-8').then(fixProcessUndefined).then(fixSchedulerNameCollision),
-  fs.readFile(path.join(reactDomPath, `react-dom-client.production.js`), 'utf-8').then(fixSchedulerNameCollision),
-  fs.readFile(path.join(reactPath, `react-jsx-runtime.development.js`), 'utf-8').then(fixProcessUndefined),
-  fs.readFile(path.join(reactPath, `react-jsx-runtime.production.js`), 'utf-8'),
-  fs.readFile(path.join(schedulerPath, `scheduler.development.js`), 'utf-8').then(fixProcessUndefined),
-  fs.readFile(path.join(schedulerPath, `scheduler.production.js`), 'utf-8'),
+const [
+  reactDevelopment,
+  react,
+  reactDomDevelopment,
+  reactDom,
+  reactDomClientDevelopment,
+  reactClientDom,
+  reactJsxRuntimeDevelopment,
+  reactJsxRuntime,
+  schedulerDevelopment,
+  scheduler,
+] = await Promise.all([
+  fs.readFile(path.join(reactPath, `react.development.js`), 'utf8').then(fixProcessUndefined),
+  fs.readFile(path.join(reactPath, `react.production.js`), 'utf8'),
+  fs.readFile(path.join(reactDomPath, `react-dom.development.js`), 'utf8').then(fixProcessUndefined),
+  fs.readFile(path.join(reactDomPath, `react-dom.production.js`), 'utf8'),
+  fs.readFile(path.join(reactDomPath, `react-dom-client.development.js`), 'utf8').then(fixProcessUndefined).then(fixSchedulerNameCollision),
+  fs.readFile(path.join(reactDomPath, `react-dom-client.production.js`), 'utf8').then(fixSchedulerNameCollision),
+  fs.readFile(path.join(reactPath, `react-jsx-runtime.development.js`), 'utf8').then(fixProcessUndefined),
+  fs.readFile(path.join(reactPath, `react-jsx-runtime.production.js`), 'utf8'),
+  fs.readFile(path.join(schedulerPath, `scheduler.development.js`), 'utf8').then(fixProcessUndefined),
+  fs.readFile(path.join(schedulerPath, `scheduler.production.js`), 'utf8'),
 ]);
 
 const distDir = path.join(__dirname, '..', 'dist');
-const devPluginPath = path.join(distDir, 'plugins', author, `${name}-dev`);
+const developmentPluginPath = path.join(distDir, 'plugins', author, `${name}-dev`);
 const pluginPath = path.join(distDir, 'plugins', author, name);
 await Promise.all([
-  fs.writeFile(path.join(devPluginPath, 'react.js'), reactDev),
+  fs.writeFile(path.join(developmentPluginPath, 'react.js'), reactDevelopment),
   fs.writeFile(path.join(pluginPath, 'react.js'), react),
-  fs.writeFile(path.join(devPluginPath, 'react-dom.js'), reactDomDev),
+  fs.writeFile(path.join(developmentPluginPath, 'react-dom.js'), reactDomDevelopment),
   fs.writeFile(path.join(pluginPath, 'react-dom.js'), reactDom),
-  fs.writeFile(path.join(devPluginPath, 'react-dom-client.js'), reactDomClientDev),
+  fs.writeFile(path.join(developmentPluginPath, 'react-dom-client.js'), reactDomClientDevelopment),
   fs.writeFile(path.join(pluginPath, 'react-dom-client.js'), reactClientDom),
-  fs.writeFile(path.join(devPluginPath, 'react-jsx-runtime.js'), reactJsxRuntimeDev),
+  fs.writeFile(path.join(developmentPluginPath, 'react-jsx-runtime.js'), reactJsxRuntimeDevelopment),
   fs.writeFile(path.join(pluginPath, 'react-jsx-runtime.js'), reactJsxRuntime),
-  fs.writeFile(path.join(devPluginPath, 'react-scheduler.js'), schedulerDev),
+  fs.writeFile(path.join(developmentPluginPath, 'react-scheduler.js'), schedulerDevelopment),
   fs.writeFile(path.join(pluginPath, 'react-scheduler.js'), scheduler),
 ]);
